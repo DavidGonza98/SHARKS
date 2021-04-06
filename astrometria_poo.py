8edbd00a4427d6141ec936f8027f4b616b2a2990
David González Álvarez <lpama98@gmail.com>
2021-04-06 18:45:08 +0100

    Errorfunc and without outliers

--- astrometria_poo.py
+++ astrometria_poo.py
@@ -11,6 +11,8 @@ import smatch
 import math as mt
 import seaborn as sns
 import pylab as plt
+from scipy import optimize, stats
+import pandas as pd
 
 class astrometria():
     
@@ -24,6 +26,7 @@ class astrometria():
         self.radius= 1/3600.
 
         self.Lleno=False
+        self.matched=False
         
     def LeerArchivo(self):
         self.datos1 = Table.read(self.archivo1, format= 'fits')
@@ -95,23 +98,25 @@ class astrometria():
             nombres.append(name)
             arrays.append(self.assoc2[name])
         
-        self.unify= Table(data=arrays, names=nombres)    
+        self.unify= Table(data=arrays, names=nombres)  
+        self.matched=True
         return self.unify
 
     def DefineMain(self, datos):
         if datos == 'sharks':
-            self.MainCatalog = self.datos1
+            self.unify = self.datos1
         else:
-            self.MainCatalog = self.datos2
+            self.unify = self.datos2
 
     def Extraer_columna(self, DameColumna, matched):
         
-        if matched == 'Si':
+        if self.matched == True:
             if DameColumna in self.unify.keys():
                 print('La columna', DameColumna, 'tiene una longitud de', len(self.unify[DameColumna]), 'un maximo de', np.max(self.unify[DameColumna]),'y un minimo de', np.min(self.unify[DameColumna]))
                 return self.unify[DameColumna]
         
-        elif matched == 'No':
+        else:
+            
             if DameColumna in self.datos1.keys():
                 print('La columna', DameColumna, 'tiene una longitud de', len(self.datos1[DameColumna]), 'un maximo de', np.max(self.datos1[DameColumna]),'y un minimo de', np.min(self.datos1[DameColumna]))
                 return self.datos1[DameColumna]
@@ -119,9 +124,9 @@ class astrometria():
             if DameColumna in self.datos2.keys():
                 print('La columna', DameColumna, 'tiene una longitud de', len(self.datos2[DameColumna]), 'un maximo de', np.max(self.datos2[DameColumna]),'y un minimo de', np.min(self.datos2[DameColumna]))
                 return self.datos2[DameColumna]
-
-        else:
-            print ('El nombre de la columna ingresada no se encuentra ningun catalogo')
+            
+            else:
+                print ('El nombre de la columna ingresada no se encuentra ningun catalogo')
             
 
 
@@ -147,17 +152,17 @@ class astrometria():
     def __makeCondition(self, condicion):
         
         if condicion[1] == 'greater':
-            return np.greater(self.Extraer_columna(condicion[0], 'Si'), condicion[2])
+            return np.greater(self.Extraer_columna(condicion[0], True), condicion[2])
         elif condicion[1] == 'greater_equal':
-            return np.greater_equal(self.Extraer_columna(condicion[0], 'Si'), condicion[2])
+            return np.greater_equal(self.Extraer_columna(condicion[0], True), condicion[2])
         elif condicion[1] == 'less':
-            return np.less(self.Extraer_columna(condicion[0], 'Si'), condicion[2])
+            return np.less(self.Extraer_columna(condicion[0], True), condicion[2])
         elif condicion[1] == 'less_equal':
-            return np.less_equal(self.Extraer_columna(condicion[0], 'Si'), condicion[2])
+            return np.less_equal(self.Extraer_columna(condicion[0], True), condicion[2])
         elif condicion[1] == 'equal':
-            return np.equal(self.Extraer_columna(condicion[0], 'Si'), condicion[2])
+            return np.equal(self.Extraer_columna(condicion[0], True), condicion[2])
         elif condicion[1] == 'not_equal':
-            return np.not_equal(self.Extraer_columna(condicion[0], 'Si'), condicion[2])
+            return np.not_equal(self.Extraer_columna(condicion[0], True), condicion[2])
         else:
             return 0
 
@@ -179,17 +184,90 @@ class astrometria():
         else:
             print('File not read')
     
-    def ajuste_lineal(self, x, y):
+    def ajuste_lineal(self, magnitud1, magnitud2, quitarOutliers=False):
         
-        #print(self.datos1[x])
+                
+        self.p=plt.polyfit(self.unify_mask[magnitud1], self.unify_mask[magnitud2], 1)
+        Y=self.p[0]*self.unify_mask[magnitud1] + self.p[1] 
+        plt.plot(self.unify_mask[magnitud1], Y)
+        plt.plot(self.unify_mask[magnitud1], self.unify_mask[magnitud2], '.')
+        print ('El valor de la pendiente del ajuste lineal es', self.p[0], 'y su ordenada en el origen', self.p[1]  )
         
-        p=plt.polyfit(self.unify_mask[x], self.unify_mask[y], 1)
-        Y=p[0]*self.unify_mask[x] + p[1] 
-        plt.plot(self.unify_mask[x], Y)
-        plt.plot(self.unify_mask[x], self.unify_mask[y], '.')
-        print ('El valor de la pendiente del ajuste lienal es', p[0], 'y su ordenada en el origen', p[1]  )
         
+            #blablabla
+            #self.p (equivalente a m,b)
+            
+    def errorfunc(self, magnitud1, magnitud2, errormagnitud1, quitarOutliers=False, pendienteuno=False):
         
+        fitfunc = lambda p, x: p[0] + p[1] * x
+        errfunc = lambda p, x, y, err: (y - fitfunc(p, x)) / err
+        
+        if self.p.any(None):
+            print ('Todo esta bien')
+
+        else:
+            self.ajuste_lineal(magnitud1, magnitud2)
+
+        #pinit = [self.p[1],self.p[0]]
+        pinit = [1,1]
+        out = optimize.leastsq(errfunc, pinit, args=(self.unify_mask[magnitud1], self.unify_mask[magnitud2],  self.unify_mask[errormagnitud1]), full_output=1)
+        #print(pinit)
+        self.pfinal = out[0]
+        
+        ajuste_con_error2= self.pfinal[1]*self.unify_mask[magnitud1] + self.pfinal[0]
+        
+        if quitarOutliers:
+            z = (self.unify_mask[magnitud2] - ajuste_con_error2)/self.unify_mask[errormagnitud1]
+
+            pf = pd.DataFrame(zip(self.unify_mask[magnitud2], self.unify_mask[magnitud1], self.unify_mask[errormagnitud1]))
+            pf_sin_outliers = pf[(np.abs(z) < 2.5)]
+            
+            m, b = plt.polyfit(pf_sin_outliers[1], pf_sin_outliers[0], 1)
+            plt.plot(pf_sin_outliers[1], m*pf_sin_outliers[1]+b, 'r-')
+            plt.plot(pf_sin_outliers[1], pf_sin_outliers[0], 'b.')
+            
+            pinit = [1,1]
+            out_sin_outliers = optimize.leastsq(errfunc, pinit, args=(pf_sin_outliers[1], pf_sin_outliers[0],  pf_sin_outliers[2]), full_output=1)
+            self.pfinal_sin_outliers= out_sin_outliers[0]
+            ajuste_sin_outliers= self.pfinal_sin_outliers[1]*pf_sin_outliers[1] + self.pfinal_sin_outliers[0]
+            
+            plt.figure(2)
+            plt.plot(pf_sin_outliers[1], ajuste_sin_outliers, 'm')
+            plt.plot(pf_sin_outliers[1], pf_sin_outliers[0], 'y.')
+            
+            if pendienteuno:
+                fitfunc = lambda p, x: p[0] + x
+                pinit = [1,1]
+                out = optimize.leastsq(errfunc, pinit, args=(self.unify_mask[magnitud1], self.unify_mask[magnitud2],  self.unify_mask[errormagnitud1]), full_output=1)
+                self.pfinal = out[0]
+                ajuste_con_error2= self.unify_mask[magnitud1] + self.pfinal[0]
+                z = (self.unify_mask[magnitud2] - ajuste_con_error2)/self.unify_mask[errormagnitud1]
+
+                pf = pd.DataFrame(zip(self.unify_mask[magnitud2], self.unify_mask[magnitud1], self.unify_mask[errormagnitud1]))
+                pf_sin_outliers = pf[(np.abs(z) < 2.5)]
+            
+                m, b = plt.polyfit(pf_sin_outliers[1], pf_sin_outliers[0], 1)
+                plt.figure(3)
+                plt.plot(pf_sin_outliers[1], pf_sin_outliers[1]+b, 'r-')
+                plt.plot(pf_sin_outliers[1], pf_sin_outliers[0], 'y.')
+            
+                pinit = [1,1]
+                out_sin_outliers = optimize.leastsq(errfunc, pinit, args=(pf_sin_outliers[1], pf_sin_outliers[0],  pf_sin_outliers[2]), full_output=1)
+                self.pfinal_sin_outliers= out_sin_outliers[0]
+                ajuste_sin_outliers= self.pfinal_sin_outliers[1]*pf_sin_outliers[1] + self.pfinal_sin_outliers[0]
+            
+                
+                plt.plot(pf_sin_outliers[1], ajuste_sin_outliers, 'm')
+
+        
+                
+
+            
+            
+
+
+
+
             
             
             
@@ -201,20 +279,8 @@ sharks.LeerArchivo()
 sharks.Match('ALPHA_J2000', 'DELTA_J2000', 'RAJ2000', 'DEJ2000')
 sharks.MainCatalog()
 
-sharks.Extraer_columna('Kmag', 'Si')
+sharks.Extraer_columna('e_Kmag', True)
       
-
-#sharks.DefineMain('Sharks') #o podrias poner '2mass'
-
-
-
-'''
-    #self.MainCatalog = union assoc1 y assoc2
-1) crear un dictionario nuevo vacio.
-2) Leer las columnas de sharks y añadirlas al nuevo
-3) leer las columnas de 2mass y añadirlas al nuevo
-
-'''
 
 
 #sharks.Matches('MAG_AUTO', 'MAGERR_AUTO', 'Kmag', 'e_Kmag')       
@@ -227,11 +293,12 @@ sharks.Extraer_columna('Kmag', 'Si')
 #plot = sharks.Histograma('Distancia angular')   
 #plt.savefig('dist_angular.png')
 
-sharks.mascara([['MAG_AUTO','greater_equal',12.3],['MAGERR_AUTO','greater',0.]])
+sharks.mascara([['MAG_AUTO','greater_equal',12.3],['MAGERR_AUTO','greater',0.], ['e_Kmag','greater',0.]])
 
-sharks.ajuste_lineal('MAG_AUTO', 'Kmag')  
+sharks.ajuste_lineal('Kmag', 'MAG_AUTO')  #cambiar ord
+#plt.savefig('Ajuste desde POO.png')
         
-        
+sharks.errorfunc('Kmag', 'MAG_AUTO', 'e_Kmag', True, True)       
         
         
         
