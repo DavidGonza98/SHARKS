24b3c540cb687b0c623607660085ec284e5b85f8
David González Álvarez <lpama98@gmail.com>
2021-05-21 19:09:36 +0100

    Metrical measures

--- Instancias.py
+++ Instancias.py
@@ -14,7 +14,7 @@ from scipy import optimize, stats
 import pandas as pd
 
 
-import catalogo_sharks, astrometria_poo, Fotometria
+import catalogo_sharks, astrometria_poo, Fotometria, PhotoZ
 
 '''
 sharks=Catalogo('Sharks', 'Sharks_sgp_e_2_cat_small.fits', 'ALPHA_J2000', 'DELTA_J2000')
@@ -91,7 +91,7 @@ two_mass_fotometria.ajuste_lineal('Kmag', 'APERMAG3', two_mass, sharks_sgpe)
 
 two_mass_fotometria.errorfunc('Kmag', 'APERMAG3', 'e_Kmag', two_mass, sharks_sgpe, True, True)
 '''
-
+'''
 des=catalogo_sharks.Catalogo('DES', 'des_in_field.fits', 'RA', 'DEC')
 sharks_sgpe_des=catalogo_sharks.Catalogo('Sharks_sgpe', 'sharks_sgpe.fits', 'RA', 'DEC')
 
@@ -119,7 +119,7 @@ sharks_sgpe_des.correctExtinction(['PETROFLUX','PETROFLUXERR'],'flux','Ks')
 
 sharks_sgpe_des.saveSample(['COADD_OBJECT_ID','RA','DEC', 'FLUX_AUTO_CORRECTED_G', 'FLUXERR_AUTO_CORRECTED_G', 'FLUX_AUTO_CORRECTED_R', 'FLUXERR_AUTO_CORRECTED_R', 'FLUX_AUTO_CORRECTED_I', 'FLUXERR_AUTO_CORRECTED_I', 'FLUX_AUTO_CORRECTED_Z', 'FLUXERR_AUTO_CORRECTED_Z', 'FLUX_AUTO_CORRECTED_Y', 'FLUXERR_AUTO_CORRECTED_Y', 'FLUX_APER_CORRECTED', 'FLUXERR_APER_CORRECTED'])
 sharks_sgpe_des.createSample(format='basic', nameSample='Galaxias_dessharks')
-
+'''
 '''
 sharks_sgpe_des_astrometria=astrometria_poo.astrometria(sharks_sgpe_des)
 sharks_sgpe_des_astrometria.Distancia_Angular()
@@ -133,12 +133,11 @@ sharks_sgpe_des_astrometria.plot('X')
 #sharks_sgpe_des_astrometria.plot('RA')
 #sharks_sgpe_des_astrometria.plot('DEC')
 
-
-
-
-
 sharks_sgpe_des_astrometria.plot('L')
 '''
 
+results_eazy = PhotoZ.PhotoZ('eazy', ['gals_dessharks_1.txt', 'gals_dessharks_2.txt', 'gals_dessharks_3.txt', 'gals_dessharks_4.txt', 'gals_dessharks_5.txt'], 'sharks')
 
+results_eazy.getStats()  
+results_eazy.getStats_Bin([0.,0.1,0.2,0.3,0.4,0.5])
 
--- PhotoZ.py
+++ PhotoZ.py
@@ -26,8 +26,9 @@ class PhotoZ():
         None.
 
         """
-        
-        
+        self.codeName=codeName
+        self.parentList=parentList
+        self.tag=tag
         self.nombresalida = 'mytest'
         self.__Prepare()
         self.__mergeResults()
@@ -48,18 +49,18 @@ class PhotoZ():
         self.params = []
         self.datafits = []
         for f in self.parentList:
-            self.zout_results.append(f + self.tag + '.zout.fits')
-            self.zero_points.append(f + self.tag + '.zphot.zeropoint')
-            self.translate.append(f + self.tag + '.zphot.translate')
-            self.params.append(f + self.tag + '.zphot.param')
-            self.datafits.append(f + self.tag + '.data.fits')
+            self.zout_results.append(f +'.'+ self.tag + '.zout.fits')
+            self.zero_points.append(f + '.'+self.tag + '.zphot.zeropoint')
+            self.translate.append(f +'.'+ self.tag + '.zphot.translate')
+            self.params.append(f +'.'+ self.tag + '.zphot.param')
+            self.datafits.append(f +'.'+ self.tag + '.data.fits')
             
             
     def unify_data(self):
         import astropy.io.fits as pf
         import numpy as np
 
-        files = ['gals_des_1.txt.eazypy.data.fits','gals_des_2.txt.eazypy.data.fits','gals_des_3.txt.eazypy.data.fits','gals_des_4.txt.eazypy.data.fits','gals_des_5.txt.eazypy.data.fits']
+        files = self.datafits
         hdul = pf.open(files[0])
         primary_hdu = hdul[0]
 
@@ -93,7 +94,7 @@ class PhotoZ():
 
         hd_out = pf.HDUList([primary_hdu, zbest_hdu, zgrid_hdu, chi_hdu, coeffs_hdu])
 
-        hd_out.writeto('test.data.fits')
+        hd_out.writeto(self.nombresalida+'.data.fits')
 
 
         
@@ -110,7 +111,7 @@ class PhotoZ():
 
         """
         from astropy.table import Table, vstack
-        from astropy.io import writeto
+        import astropy.io.fits
         lista_temp = []
         for f in  self.zout_results:
             
@@ -118,35 +119,89 @@ class PhotoZ():
             lista_temp.append(t)
             
         self.mergeZout = vstack(lista_temp)
-        self.mergeZout.writeto('Zout.fits')
+        self.mergeZout.write(self.nombresalida + 'Zout.fits', overwrite=True)
         
     def getStats(self):
-        import numpy as np  
-        
-        self.zspec_original=self.mergeZout['zspec']
+        import numpy as np 
+        from astropy.table import Table, vstack
+        import astropy.io.fits
+
+ 
+        self.zspec_original= self.mergeZout['z_spec']
+        mask = (self.zspec_original>0)
+        self.zspec = self.zspec_original[mask]
+        self.zphot= self.mergeZout['z_phot'][mask]
+        '''
         self.zspec=[]
-        if np.greater_equal(self.zspec_original, 0.0):
-            self.zspec.append(self.zspec_original)
-            
-        self.zphot= self.mergeZout['zphot']
-        
-        
+        if np.greater_equal(self.zspec_original, 0.0) :
+            if self.zspec_original.all()!=-1:
+                self.zspec.append(self.zspec_original.all())
+        self.zspec=np.array(self.zspec)
+        print(self.zspec)
+        self.zphot= self.mergeZout['z_phot']
+        '''
+        self.arr=(self.zspec-self.zphot)
         self.zbias= np.mean(self.delta_z_1pz())
         
-        self.NMAD=self.arr/(1+self.zspec)
-        self.outliers=self.arr
+        self.NMAD = np.mean(np.abs(self.arr/(1+self.zspec))) 
+        self.outliers=self.outlier_rate(self.arr)
         
-        self.t=self.sigma68/(1+self.NMAD)
+        arr = self.arr/(1+self.zspec)
+        
+        self.t=self.sigma_68(arr)
+        
+        print('zbias',self.zbias)
+        print('Outliers', self.outliers)
+        print ('sigma_68', self.t)
+        print('NMAD', self.NMAD)
+        
+    def getStats_Bin(self, lims):
         
         
+        import numpy as np
+        import matplotlib.pyplot as plt
+        
+        #lims = [0.,0.1,0.2,0.3,0.4,0.5]
+        self.zspec_original= self.mergeZout['z_spec']
+        self.zphot= self.mergeZout['z_phot']
+
+        bin_central = []
+        sigma68 = []
+        outliers = []
+        zbias = []
+        NMAD = []
+        for i in range(len(lims)-1):
+            mask = (self.zspec_original>lims[i])&(self.zspec_original<lims[i+1])
+
+            zspec_tem = self.zspec_original[mask]
+            zphot_tem = self.zphot[mask]
+            bin_central.append((lims[i]+lims[i+1])/2.)
+            arr = (zspec_tem-zphot_tem)/(1+zspec_tem)
+            zbias.append(np.mean(zspec_tem-zphot_tem))
+            outliers.append(self.outlier_rate(zspec_tem-zphot_tem))
+            sigma68.append(self.sigma_68(arr))
+            NMAD.append(np.mean(np.abs((zspec_tem-zphot_tem)/(1+zspec_tem))))
+            #obtienes zbias, sigma68 y outliers
+        
+        # 1 plot para metrica, para zbias, sigma68 y outliers, donde eje x = bin_central, y = metrica
+        plt.plot(bin_central, zbias, label='zbias')
+        plt.plot(bin_central, sigma68, label='sigma68')
+        plt.plot(bin_central, outliers, label='outliers')
+        plt.plot(bin_central, NMAD, label='NMAD')
+        plt.xlabel('zspec') 
+        plt.ylabel('zphot')
+        plt.title('Metrical measures')
+        plt.legend()
+        plt.savefig('Metrical_measures.png')
+            
     def delta_z(self):
-        self.arr=(self.zspec-self.zphot)
+        
         return self.arr
     
     def delta_z_1pz(self):
         return self.arr/(1+self.zspec)
     
-    def sigma_68(arr, axis=None):
+    def sigma_68(self, arr, axis=None):
         """Input: an (multi-dimensional) array
            Optional input: the axis along which to calculate the metric
            Outputs: the 68% spread of data about the median value of the array
@@ -154,12 +209,22 @@ class PhotoZ():
         import numpy as np 
         upper, lower = np.percentile(arr, [84.075, 15.825], axis=axis)
         return (upper - lower) / 2.0
+    
+    def outlier_rate(self, arr, outR=None):
+        """assumes frac outliers >0.15"""
+        import numpy as np
+    
+        if outR is None:
+            outR = 0.15
+        return np.sum(np.abs(arr) > outR)*1.0/len(arr)
+        
+        
+'''        
+results_eazy = PhotoZ('eazy', ['gals_dessharks_1.txt', 'gals_dessharks_2.txt', 'gals_dessharks_3.txt', 'gals_dessharks_4.txt', 'gals_dessharks_5.txt'], 'sharks')
 
-        
-        
-        
-        
-        
-        
+results_eazy.getStats()  
+results_eazy.getStats_Bin([0.,0.1,0.2,0.3,0.4,0.5])    
+#results_eazy.delta_z_1pz()        
+'''        
         
 
